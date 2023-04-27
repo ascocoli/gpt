3be6cc265d4e04e35b4c928071eb6b820d3258a4
ascocoli <anassetiendrebeogo77@gmail.com>
2023-04-27 11:30:48 +0000

    speech

--- lib/controllers/chats.dart
+++ lib/controllers/chats.dart
@@ -43,7 +43,8 @@ class Chats extends GetxController {
   }
 
   final openAI = OpenAI.instance.build(
-      token: "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",
+      token:
+          "sk-itJzVwfSIIQMAJfa9MXDT3BlbkFJeAnRXidwPpfkf84dWAoqana-itJzVwfSIIQMAJfa9MXDT3BlbkFJ",
       baseOption: HttpSetup(receiveTimeout: const Duration(seconds: 20)),
       isLog: true);
   void _onFocusChange() {
