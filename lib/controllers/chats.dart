4a8a0a110634d6c9695afe8f11e086b869d0be5f
ascocoli <anassetiendrebeogo77@gmail.com>
2023-04-27 09:13:34 +0000

    one

---
+++ lib/controllers/chats.dart
@@ -0,0 +1,141 @@
+import 'dart:async';
+import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
+
+class Chats extends GetxController {
+  ScrollController scrollTobottom = ScrollController();
+  RxBool isText = false.obs;
+  FocusNode chatFocus = FocusNode();
+
+  TextEditingController chatCtl = TextEditingController();
+  RxList<List<Map<String, String>>> chats = [
+    [
+      {
+        "role": "gpt",
+        "content": "BonJour ! Comment je peux vous aidez ?",
+      },
+      {"finish_reason": "top"},
+    ],
+  ].obs;
+  //for speech variables
+  SpeechToText speechToText = SpeechToText();
+  RxBool speechEnabled = true.obs;
+  RxString lastWords = ''.obs;
+
+  sendQ(q) {
+    if (chats.last[0]["content"] != q) {
+      chats.add([
+        {
+          "role": "user",
+          "content": q,
+        },
+        {"finish_reason": "top"},
+      ]);
+    }
+
+    chatComplete(q);
+    scrollTobottom.animateTo(scrollTobottom.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 500), curve: Curves.easeIn);
+  }
+
+  final openAI = OpenAI.instance.build(
+      token: "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",
+      baseOption: HttpSetup(receiveTimeout: const Duration(seconds: 20)),
+      isLog: true);
+  void _onFocusChange() {
+    isText.value = chatFocus.hasFocus;
+  }
+
+  void chatComplete(question) async {
+    final request = ChatCompleteText(
+      messages: [
+        Map.of({"role": "user", "content": question}),
+      ],
+      maxToken: 200,
+      model: ChatModel.gptTurbo0301,
+    );
+
+    final response = await openAI.onChatCompletion(request: request);
+    if (response!.choices.isNotEmpty) {
+      chatCtl.clear();
+      chats.add([
+        {
+          "role": "gpt",
+          "content": response.choices[0].message!.content,
+        },
+        {"finish_reason": "top"},
+      ]);
+    }
+  }
+
+  goToBottom() {
+    scrollTobottom.animateTo(scrollTobottom.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 500), curve: Curves.easeIn);
+  }
+
+  // for speech text
+  void initSpeech() async {
+    speechEnabled.value = await speechToText.initialize(
+      onError: (e) {
+        print("error $e");
+      },
+      onStatus: (e) {
+        print("state $e");
+      },
+    );
+
+    // final PermissionStatus permis = await _getPhonePermission();
+  }
+
+  /// Each time to start a speech recognition session
+  void startListening() async {
+    print("hello");
+
+    await speechToText.listen(onResult: _onSpeechResult);
+  }
+
+  /// Manually stop the active speech recognition session
+  /// Note that there are also timeouts that each platform enforces
+  /// and the SpeechToText plugin supports setting timeouts on the
+  /// listen method.
+  void stopListening() async {
+    // print("Voici le mot${lastWords}");
+    await speechToText.stop();
+    Timer(const Duration(seconds: 2), () {
+      sendQ(lastWords.value);
+    });
+
+    speechEnabled.value = false;
+  }
+
+  /// This is the callback that the SpeechToText plugin calls when
+  /// the platform returns recognized words.
+  void _onSpeechResult(SpeechRecognitionResult result) {
+    lastWords.value = result.recognizedWords;
+    print(result.recognizedWords);
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+    initSpeech();
+    chatFocus.addListener(_onFocusChange);
+  }
+
+  @override
+  void onClose() {
+    // TODO: implement onClose
+    chatFocus.removeListener(_onFocusChange);
+    chatFocus.dispose();
+  }
+}
+
+
+//{role: assistant, content: Je suis une IA et je ne peux pas écrire de code. Mais voici le code "Hello World" en PHP :
+// <?php
+// echo "Hello World!";
+// ?>}, finish_reason: stop}
---
+++ lib/widgets/chatswidget.dart
@@ -0,0 +1,136 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:gpt/controllers/chats.dart';
+import 'package:gpt/controllers/speech.dart';
+
+class ChatsWidget extends StatelessWidget {
+  ChatsWidget({
+    super.key,
+  });
+  final chat = Get.put(Chats());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text("GPT"),
+      ),
+      body: Obx(
+        () => SingleChildScrollView(
+          controller: chat.scrollTobottom,
+          child: Column(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    // Get.to(Speech());
+                  },
+                  child: Text("next")),
+              ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: chat.chats.length,
+                itemBuilder: (context, index) => Column(
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: chat.chats[index][0]["role"] == "gpt"
+                              ? Colors.black
+                              : Colors.blue,
+                          child: Text(chat.chats[index][0]["role"]!),
+                        ),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: index == chat.chats.length - 1 && index >= 1
+                                ? Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      chat.chats[index][0]["role"] == "gpt"
+                                          ? AnimatedTextKit(
+                                              onNext: (o, oo) =>
+                                                  chat.goToBottom(),
+                                              onFinished: () =>
+                                                  chat.goToBottom(),
+                                              isRepeatingAnimation: false,
+                                              // totalRepeatCount: 1,
+                                              animatedTexts: [
+                                                  TypewriterAnimatedText(
+                                                      "...."),
+                                                  TypewriterAnimatedText(
+                                                      chat.chats[index][0]
+                                                          ["content"]!),
+                                                ])
+                                          : SelectableText(
+                                              chat.chats[index][0]["content"]!),
+                                    ],
+                                  )
+                                : SelectableText(
+                                    chat.chats[index][0]["content"]!),
+                          ),
+                        )
+                      ],
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 90, right: 90),
+                      child: Divider(),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 60,
+              )
+            ],
+          ),
+        ),
+      ),
+      bottomSheet: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: Container(
+          decoration: BoxDecoration(
+              border: Border.all(color: Colors.blue),
+              borderRadius: const BorderRadius.all(Radius.circular(15))),
+          child: Obx(
+            () => Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    focusNode: chat.chatFocus,
+                    onSubmitted: (value) => chat.sendQ(chat.chatCtl.text),
+                    controller: chat.chatCtl,
+                    maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                    decoration: const InputDecoration(
+                      hintText: "Demarer une converstion",
+                    ),
+                  ),
+                ),
+                chat.isText.isFalse
+                    ? IconButton(
+                        onPressed: () {
+                          chat.speechToText.isListening
+                              ? chat.stopListening()
+                              : chat.startListening();
+                        },
+                        icon: chat.speechEnabled.isTrue
+                            ? const Icon(Icons.pause_circle_filled_rounded,
+                                color: Colors.green)
+                            : const Icon(Icons.mic, color: Colors.blue))
+                    : IconButton(
+                        onPressed: () {
+                          chat.sendQ(chat.chatCtl.text);
+                          Get.back();
+                        },
+                        icon: const Icon(Icons.send))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
